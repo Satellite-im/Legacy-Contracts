90f78e9b4611959724da3058a7d8744d292d5ed0
mtumiati <manuel.tumiati@knobs.it>
2021-04-04 22:53:09 +0200

    Update sticker factory contract to easily track creators of sticker sets and added restrictions for the sticker creation

--- test/3_stickerFactory.js
+++ test/3_stickerFactory.js
@@ -11,12 +11,47 @@ contract("StickerFactory", (accounts) => {
     assert.notEqual(instance, null);
   });
 
+  it("Should register a new artist", async function () {
+    const factoryInstance = await StickerFactory.deployed();
+
+    let error = null;
+    await factoryInstance
+      .registerArtist(
+        accounts[1],
+        "Banksy",
+        "Qmaz17CGuuo2gU9V9VW3fHs49ufGVA1hvr3e7r4fonzcsG",
+        "You don't know me, but I know you."
+      )
+      .catch((e) => {
+        error = e;
+      });
+
+    assert.equal(error, null, "A new artist was registered");
+  });
+
+  it("It should be possible to get the artist details", async function () {
+    const factoryInstance = await StickerFactory.deployed();
+
+    let error = null;
+    const artist = await factoryInstance.artists(accounts[1]).catch((e) => {
+      error = e;
+    });
+    assert.equal(error, null, "It is not possible to get artist details");
+    assert.equal(
+      artist.name,
+      "Banksy",
+      "It is not possible to get artist name"
+    );
+  });
+
   it("Users should be able to create a new sticker", async function () {
     const instance = await StickerFactory.deployed();
 
     const setsBefore = await instance.getAvailableSets();
 
-    await instance.createSticker("MySticker", "MyS", 2, "http://", price);
+    await instance.createSticker("MySticker", "MyS", 2, "http://", price, {
+      from: accounts[1],
+    });
 
     const setsAfter = await instance.getAvailableSets();
 
@@ -31,9 +66,10 @@ contract("StickerFactory", (accounts) => {
     const factoryInstance = await StickerFactory.deployed();
     const sets = await factoryInstance.getAvailableSets();
     assert(sets.length > 0, "No sets available. Unable to proceed with test");
-    const instance = await Sticker.at(sets[0]);
+    const stickerContractAddress = sets[0].stickerContract;
+    const instance = await Sticker.at(stickerContractAddress);
 
-    const balance = await instance.balanceOf(accounts[0]);
+    const balance = await instance.balanceOf(accounts[1]);
 
     assert.equal(
       balance,
@@ -46,20 +82,21 @@ contract("StickerFactory", (accounts) => {
     const factoryInstance = await StickerFactory.deployed();
     const sets = await factoryInstance.getAvailableSets();
     assert(sets.length > 0, "No sets available. Unable to proceed with test");
-    const instance = await Sticker.at(sets[0]);
+    const stickerContractAddress = sets[0].stickerContract;
+    const instance = await Sticker.at(stickerContractAddress);
 
-    const balanceBefore = await instance.balanceOf(accounts[1]);
+    const balanceBefore = await instance.balanceOf(accounts[2]);
 
     let error = null;
     await instance
-      .addSet({ from: accounts[1], value: lowerPrice })
+      .addSet({ from: accounts[2], value: lowerPrice })
       .catch((e) => {
         error = e;
       });
 
     assert.notEqual(error, null, "Sticker has been bought anyway");
 
-    const balanceAfter = await instance.balanceOf(accounts[1]);
+    const balanceAfter = await instance.balanceOf(accounts[2]);
 
     assert.notEqual(
       balanceBefore,
@@ -75,13 +112,14 @@ contract("StickerFactory", (accounts) => {
       sets.length > 0,
       "No sticker available. Unable to proceed with test"
     );
-    const instance = await Sticker.at(sets[0]);
+    const stickerContractAddress = sets[0].stickerContract;
+    const instance = await Sticker.at(stickerContractAddress);
 
-    const balanceBefore = await instance.balanceOf(accounts[1]);
+    const balanceBefore = await instance.balanceOf(accounts[2]);
 
-    await instance.addSet({ from: accounts[1], value: price });
+    await instance.addSet({ from: accounts[2], value: price });
 
-    const balanceAfter = await instance.balanceOf(accounts[1]);
+    const balanceAfter = await instance.balanceOf(accounts[2]);
 
     assert.notEqual(
       balanceBefore,
@@ -94,10 +132,11 @@ contract("StickerFactory", (accounts) => {
     const factoryInstance = await StickerFactory.deployed();
     const sets = await factoryInstance.getAvailableSets();
     assert(sets.length > 0, "No sets available. Unable to proceed with test");
-    const instance = await Sticker.at(sets[0]);
+    const stickerContractAddress = sets[0].stickerContract;
+    const instance = await Sticker.at(stickerContractAddress);
 
     let error = null;
-    await instance.addSet({ from: accounts[2], value: price }).catch((e) => {
+    await instance.addSet({ from: accounts[3], value: price }).catch((e) => {
       error = e;
     });
 
@@ -112,64 +151,43 @@ contract("StickerFactory", (accounts) => {
     const factoryInstance = await StickerFactory.deployed();
     const sets = await factoryInstance.getAvailableSets();
     assert(sets.length > 0, "No sets available. Unable to proceed with test");
-    const instance = await Sticker.at(sets[0]);
 
-    const balanceBefore = await web3.eth.getBalance(accounts[0]);
+    const stickerContractAddress = sets[0].stickerContract;
 
-    const contractBalanceBefore = await web3.eth.getBalance(sets[0]);
+    const instance = await Sticker.at(stickerContractAddress);
+
+    const contractBalanceBefore = await web3.eth.getBalance(stickerContractAddress);
 
     let error = null;
-    await instance.claim().catch((e) => {
+    await instance.claim({from: accounts[1]}).catch((e) => {
       error = e;
-      console.log(e);
     });
 
-    const balanceAfter = await web3.eth.getBalance(accounts[0]);
+    const contractBalanceAfter = await web3.eth.getBalance(stickerContractAddress);
 
-    const contractBalanceAfter = await web3.eth.getBalance(sets[0]);
-
-    assert.equal(balanceBefore < balanceAfter, true, "Creator balance didn't increase");
-    assert.equal(contractBalanceBefore > contractBalanceAfter, true, "Contract balance didn't change");
+    assert.equal(
+      contractBalanceBefore > contractBalanceAfter,
+      true,
+      "Contract balance didn't change"
+    );
   });
 
   it("Only creator can claim", async function () {
     const factoryInstance = await StickerFactory.deployed();
     const sets = await factoryInstance.getAvailableSets();
     assert(sets.length > 0, "No sets available. Unable to proceed with test");
-    const instance = await Sticker.at(sets[0]);
+    const stickerContractAddress = sets[0].stickerContract;
+    const instance = await Sticker.at(stickerContractAddress);
 
     let error = null;
-    await instance.claim({from: accounts[3]}).catch((e) => {
+    await instance.claim({ from: accounts[4] }).catch((e) => {
       error = e;
     });
 
-    assert.notEqual(error, null, "A user that was not a creator was able to claim funds");
-  });
-
-  it("Should register a new artist", async function () {
-    const factoryInstance = await StickerFactory.deployed();
-
-    let error = null;
-    await factoryInstance.registerArtist(
-      "0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7",
-      "Banksy", 
-      "Qmaz17CGuuo2gU9V9VW3fHs49ufGVA1hvr3e7r4fonzcsG", 
-      "You don't know me, but I know you."
-    ).catch((e) => {
-      error = e;
-    });
-
-    assert.equal(error, null, "A new artist was registered");
-  });
-
-  it("It should be possible to get the artist details", async function () {
-    const factoryInstance = await StickerFactory.deployed();
-
-    let error = null;
-    const artist = await factoryInstance.artists("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7").catch((e) => {
-      error = e;
-    });
-    assert.equal(error, null, "It is not possible to get artist details");
-    assert.equal(artist.name, 'Banksy', "It is not possible to get artist name");
+    assert.notEqual(
+      error,
+      null,
+      "A user that was not a creator was able to claim funds"
+    );
   });
 });
